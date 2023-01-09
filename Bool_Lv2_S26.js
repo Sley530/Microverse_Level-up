653a2117a0f0c968ac1795f4cbdf89deefd378cb
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-08 21:19:17 -0500

    Update

---
+++ Bool_Lv2_S26.js
@@ -0,0 +1,24 @@
+// Number 1
+function welcomeToBooleans() {
+    // Only change code below this line
+  
+    return true; // Change this line
+  
+    // Only change code above this line
+  }
+
+
+// // Number 2
+// function trueOrFalse(wasThatTrue) {
+//     // Only change code below this line
+//     if (wasThatTrue)
+//     {
+//       return "Yes, that was true";
+//     }
+//    return "No, that was false";
+  
+//     // Only change code above this line
+//   }
+  
+  
+  // Number 3
